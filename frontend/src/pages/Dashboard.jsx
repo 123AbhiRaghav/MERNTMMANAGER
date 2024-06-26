954942be6e7b71a054b14182d7f0ccd9bb8b7ba4
123AbhiRaghav <abhiraghavd@gmail.com>
2024-06-26 19:44:21 +0530

    update

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -188,6 +188,27 @@ const Dashboard = () => {
     },
   ];
 
+  const Card = ({ label, count, bg, icon }) => {
+    return (
+      <div className='w-full h-32 bg-white p-5 shadow-md rounded-md flex items-center justify-between'>
+        <div className='h-full flex flex-1 flex-col justify-between'>
+          <p className='text-base text-gray-600'>{label}</p>
+          <span className='text-2xl font-semibold'>{count}</span>
+          <span className='text-sm text-gray-400'>{"110 last month"}</span>
+        </div>
+
+        <div
+          className={clsx(
+            "w-10 h-10 rounded-full flex items-center justify-center text-white",
+            bg
+          )}
+        >
+          {icon}
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="h-full py-4">
       <div className="grid grid-cols-1 md:grid-cols-4 gap-5">
